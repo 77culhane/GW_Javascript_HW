a4488d2d2a49bbaa3f41b4fe66eb05142e34b8bc
John <77culhane@cua.edu>
2020-01-24 17:59:04 -0500

    update

--- 14-Intro-To-JavaScript/Mon-Wed-Class/Part-2/Instructions/StarterCode/static/js/app.js
+++ 14-Intro-To-JavaScript/Mon-Wed-Class/Part-2/Instructions/StarterCode/static/js/app.js
@@ -1,16 +1,23 @@
 // from data.js
 var tableData = data;
-
-var tablebody = d3.select("tbody");
-
-function loaddata(data) {
+//define tablewriting function
+function tablewriter(tableData) {
+    //from the empty <tbody> tag in index.html
+    var tablebody = d3.select("tbody");
+    //from the "Enter a Date" label in index.html
+    var date = d3.select("#datetime").property("value");
+    //begin looping through objects within array "data"
     for (let i = 0; i < data.length; i++) {
+        //hold values of array at index "i" within variable "rowvalues"
+        var rowvalues = Object.values(tableData[i])
+        //append a row tag <tr> to <tbody>
         var datarow = tablebody.append("tr");
-        var j = Object.values(data[i])
-        for (let k = 0; k < j.length; k++) {
-            datarow.append("td").text(j[k]);
+        //begin looping through key values within array at indexx "i"
+        for (let j = 0; j < rowvalues.length; j++) {
+            //append a value tag <td> to <tr>
+            datarow.append("td").text(rowvalues[j]);
             }
         ;
     };
 }
-loaddata(tableData);
+tablewriter(tableData);
