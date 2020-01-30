0f2458c8169815fca863693c7793b61a66dcdfaa
John <77culhane@cua.edu>
2020-01-30 17:37:30 -0500

    fixed UFOlevel1 filter problem

--- 14-Intro-To-JavaScript/Mon-Wed-Class/Part-2/Instructions/UFO-level-1 COMPLETED/StarterCode/static/js/app.js
+++ 14-Intro-To-JavaScript/Mon-Wed-Class/Part-2/Instructions/UFO-level-1 COMPLETED/StarterCode/static/js/app.js
@@ -2,28 +2,28 @@
 var tableData = data;
 //from the empty <tbody> tag in index.html
 var tablebody = d3.select("tbody");
-//from the "Enter a Date" label in index.html
-var date = d3.select("#datetime").property("value");
 
 //define tablewriting function
 function tablewriter(tableData) {
+    tablebody.html("");
     //begin looping through objects within array "data"
     for (let i = 0; i < data.length; i++) {
         //hold values of array at index "i" within variable "rowvalues"
         var rowvalues = Object.values(tableData[i])
         //append a row tag <tr> to <tbody>
         var datarow = tablebody.append("tr");
-        //begin looping through key values within array at indexx "i"
+        //begin looping through key values within array at index "i"
         for (let j = 0; j < rowvalues.length; j++) {
             //append a value tag <td> to <tr>
             datarow.append("td").text(rowvalues[j]);
-            }
-        ;
+        };
     };
 }
 
 //define date filtering function
 function filterbydate() {
+    //from the "Enter a Date" label in index.html
+    var date = d3.select("#datetime").property("value");
     //redefine tableData whenever the filter is activated
     let filtereddata = tableData;
     //check if the input for the "Enter a Date" label from index.html was filled out
@@ -33,7 +33,7 @@ function filterbydate() {
     }
     //run the tablewriter function according to "filtereddata" rather than "tabledata"
     tablewriter(filtereddata);
-  }
+}
 
 d3.selectAll("#filter-btn").on("click", filterbydate);
 
---
+++ 14-Intro-To-JavaScript/Mon-Wed-Class/Part-2/Instructions/UFO-level-2/StarterCode/static/js/app.js
@@ -0,0 +1,60 @@
+// from data.js
+var tableData = data;
+//from the empty <tbody> tag in index.html
+var tablebody = d3.select("tbody");
+//from the "Enter a Date" label in index.html
+var date = d3.select("#datetime").property("value");
+
+//define tablewriting function
+function tablewriter(tableData) {
+    //begin looping through objects within array "data"
+    for (let i = 0; i < data.length; i++) {
+        //hold values of array at index "i" within variable "rowvalues"
+        var rowvalues = Object.values(tableData[i])
+        //append a row tag <tr> to <tbody>
+        var datarow = tablebody.append("tr");
+        //begin looping through key values within array at indexx "i"
+        for (let j = 0; j < rowvalues.length; j++) {
+            //append a value tag <td> to <tr>
+            datarow.append("td").text(rowvalues[j]);
+            }
+        ;
+    };
+}
+
+// Create object to hold multiple filter inputs
+var filters = {};
+function acceptinput() {
+  //create var to accept user input
+  var userinput = d3.select(this).select("input").property("value");
+  //create var to locate the id tag for the field in which user typed therir input
+  var idtag = d3.select(this).select("input").attr("id");
+  //check if any of the inputs from index.html were filled out
+  //if so, then add that input to the filters object at the corresponding "idtag" index
+  if (userinput) {
+    filters[idtag] = userinput;
+  }
+  //if no input was received for a particular "idtag", ignore that idtag when filtering
+  else {
+    delete filters[idtag];
+  }
+  // initiate filter function
+  filterbyinput();
+
+}
+
+function filterbyinput() {
+  //redefine tableData whenever the filter is activated
+  let filtereddata = tableData;
+  //redefine filtereddata to only include rows matching the above input for each value in "filters"
+  Object.entries(filters).forEach(([key, value]) => {
+    filtereddata = filtereddata.filter(row => row[key] === value);
+  });
+  //rewrite the table using the rows in filtereddata
+  tablewriter(filtereddata);
+}
+
+// Attach an event to listen for changes to each filter
+d3.selectAll(".filter").on("change", acceptinput);
+
+tablewriter(tableData);
